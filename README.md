328166aac2feb05b02c8437c4317aa163262f0c4
Guilherme Pereira <guilhermezemo@gmail.com>
2025-11-19 00:09:01 -0300

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Uma implementação do tabnews com o objetivo de aperfeiçoar minhas habilidades com programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <div style={{
+  return (
+    <div
+      style={{
         width: "98vw",
         height: "98vh",
         margin: 0,
@@ -8,10 +10,23 @@ function Home() {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        padding: "10px"
-    }}>
-        <h1 style={{ color: "red", fontSize: "30px", textAlign: "center", width: "50%" }}>"Amar significa amar o não amável; perdoar significa perdoar o imperdoável; e fé significa acreditar no inacreditável, ao contrário não seria virtude alguma."</h1>
-        <p style={{ color: "red", textAlign: "center" }}>-G. K. Chesterton</p>
+        padding: "10px",
+      }}
+    >
+      <h1
+        style={{
+          color: "red",
+          fontSize: "30px",
+          textAlign: "center",
+          width: "50%",
+        }}
+      >
+        "Amar significa amar o não amável; perdoar significa perdoar o
+        imperdoável; e fé significa acreditar no inacreditável, ao contrário não
+        seria virtude alguma."
+      </h1>
+      <p style={{ color: "red", textAlign: "center" }}>-G. K. Chesterton</p>
     </div>
+  );
 }
 export default Home;
